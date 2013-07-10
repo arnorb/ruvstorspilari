95ba8318da001fad193cb1d5192938d1632d4620
Arnór Bogason <arnor@arnor.is>
2013-07-10 23:49:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+README
+===
+
+A simple Chrome extension for making the player on ruv.is full width.
+
+This is enough for me. Anyone willing to add features should do so and put in a pull request.
+
+Roadmap:
+* A trigger to toggle the width of the player.
---
+++ myscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+		$('#dagskra-haegri').css('margin-top', 572);
+		$('#spilarinn').attr('width',976);
+		$('#spilarinn').attr('height',550);
+
+
+	/*$('.staekkarinn').click(function(){
+		$('#dagskra-haegri').attr('margin-top',572);
+		$('#spilarinn').attr('width',976);
+		$('#spilarinn').attr('height',550);
+	});*/
+
+});
