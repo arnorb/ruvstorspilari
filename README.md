f63ce801148443f5425fe9a4c5222a9b4e76c8f4
Arnór Bogason <arnor@arnor.is>
2013-10-25 02:40:46 +0000

    added matches and button, styles

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ README
 A simple Chrome extension for making the player on ruv.is full width.
 
 This is enough for me. Anyone willing to add features should do so and put in a pull request.
-
-Roadmap:
-* A trigger to toggle the width of the player.
--- myscript.js
+++ myscript.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
 
-		$('#dagskra-haegri').css('margin-top', 572);
-		$('#spilarinn').attr('width',976);
-		$('#spilarinn').attr('height',550);
+	var stor = false;
+
+	$('<li class="leaf staekkarinn"><a href="javascript:void(0);">Stækka spilarann</a></li>').appendTo('.undir-valmynd .menu');
+
+	$('.staekkarinn').click(function(){
+		if (!stor) {
+			$('#dagskra-haegri').css('margin-top', 572);
+			$('#sarphugavert').css('margin-top', 572);
+			$('#spilarinn').attr('width',976);
+			$('#spilarinn').attr('height',550);
+			$('.staekkarinn a').text('Minnka spilarann');
+			stor = true;
+		} else {
+			$('#dagskra-haegri').css('margin-top', 6);
+			$('#sarphugavert').css('margin-top', 6);
+			$('#spilarinn').attr('width',640);
+			$('#spilarinn').attr('height',360);
+			$('.staekkarinn a').text('Stækka spilarann');
+			stor = false;
+		}
+
+	});
 
 
-	/*$('.staekkarinn').click(function(){
-		$('#dagskra-haegri').attr('margin-top',572);
-		$('#spilarinn').attr('width',976);
-		$('#spilarinn').attr('height',550);
-	});*/
 
 });
